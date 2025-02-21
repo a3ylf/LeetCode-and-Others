7fee2c0c6d3cdb88c244e1dd1b995fed32339c01
apns <alexandre3ylf@gmail.com>
2025-02-21 09:10:37 -0300

    217 containsduplicate

---
+++ Leetcode/217_containsduplicate.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        a = set()
+        for x in nums:
+            if x in a:
+                return True
+
+            a.add(x)
+
+        return False
